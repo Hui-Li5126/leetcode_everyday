70b7037efd37ac9912703d1a96efdcc41e8bab57
Hui-Li5126 <lihui5126@gmail.com>
2021-03-01 09:53:42 -0800

    add new file

---
+++ p235_Lowest Common Ancestor of a Binary Search Tree.py
@@ -0,0 +1,21 @@
+Python 3.8.3 (default, May 19 2020, 06:50:17) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        pointer = root
+        
+        while pointer:
+            if p.val > pointer.val and q.val > pointer.val:
+                pointer = pointer.right
+            elif p.val < pointer.val and q.val < pointer.val:
+                pointer = pointer.left
+            else:
+                return pointer
+            
