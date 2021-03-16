a92bb560e6bc887998fd4da0d47b6f6bc2102269
Hui-Li5126 <lihui5126@gmail.com>
2021-03-15 21:08:33 -0700

    fix p5 error

--- p5_longest_palindromic_substring.py
+++ p5_longest_palindromic_substring.py
@@ -30,14 +30,15 @@ class Solution:
 
 
 class Solution:
-    def longestPalindrom(self, s):
+    def longestPalindrome(self, s):
         if not s:
             return ""
         answer = (0, 0)
         for mid in range(len(s)):
             answer = max(answer, self.get_palindrome_from(s, mid, mid))
             answer = max(answer, self.get_palindrome_from(s, mid, mid + 1))
-    
+        return s[answer[1]: answer[0] + answer[1]]
+        
     def get_palindrome_from(self, s, left, right):
         while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
