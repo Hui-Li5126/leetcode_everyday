97e1217ef0818b53c6ca60d76a37e0c0874bd8f2
Hui-Li5126 <lihui5126@gmail.com>
2021-03-17 21:26:13 -0700

    adding new one

---
+++ lintcode_31_partition_array.py
@@ -0,0 +1,22 @@
+Python 3.8.3 (default, May 19 2020, 06:50:17) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> class Solution:
+    """
+    @param nums: The integer array you should partition
+    @param k: An integer
+    @return: The index after partition
+    """
+    def partitionArray(self, nums, k):
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            while left <= right and nums[left] < k:
+                left += 1
+            while left <= right and nums[right] >= k:
+                right -= 1
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+        
+        return left
