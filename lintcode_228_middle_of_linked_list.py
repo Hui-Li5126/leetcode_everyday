51c615130a4a4355d0222a103eeae76839343aca
Hui-Li5126 <lihui5126@gmail.com>
2021-03-17 21:14:49 -0700

    adding new one

---
+++ lintcode_228_middle_of_linked_list.py
@@ -0,0 +1,24 @@
+Python 3.8.3 (default, May 19 2020, 06:50:17) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> """
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    # @param head: the head of linked list.
+    # @return: a middle node of the linked list
+    def middleNode(self, head):
+        # Write your code here
+        if head is None:
+            return None
+        slow = head
+        fast = slow.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
