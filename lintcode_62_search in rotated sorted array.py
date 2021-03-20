abf95f8b6bc0b8bb1f56d8e1d944d78dd86dbec4
Hui-Li5126 <lihui5126@gmail.com>
2021-03-19 20:18:22 -0700

    adding new one

---
+++ lintcode_62_search in rotated sorted array.py
@@ -0,0 +1,31 @@
+Python 3.8.3 (default, May 19 2020, 06:50:17) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> class Solution:
+    """
+    @param A: an integer rotated sorted array
+    @param target: an integer to be searched
+    @return: an integer
+    """
+    def search(self, A, target):
+        if not A:
+            return -1
+            
+        start, end = 0, len(A) - 1
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if A[mid] >= A[start]:
+                if A[start] <= target <= A[mid]:
+                    end = mid
+                else:
+                    start = mid
+            else:
+                if A[mid] <= target <= A[end]:
+                    start = mid
+                else:
+                    end = mid
+                    
+        if A[start] == target:
+            return start
+        if A[end] == target:
+            return end
+        return -1
