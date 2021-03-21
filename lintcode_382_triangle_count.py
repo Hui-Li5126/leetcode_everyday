413602b28a4b6388c7d847d79285cb86d2118a3e
Hui-Li5126 <lihui5126@gmail.com>
2021-03-21 14:17:58 -0700

    adding new one

---
+++ lintcode_382_triangle_count.py
@@ -0,0 +1,21 @@
+Python 3.8.3 (default, May 19 2020, 06:50:17) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> class Solution:
+    """
+    @param S: A list of integers
+    @return: An integer
+    """
+    def triangleCount(self, S):
+        # write your code here
+        S.sort()
+    
+        ans = 0
+        for i in range(len(S)):
+            left, right = 0, i - 1
+            while left < right:
+                if S[left] + S[right] > S[i]:
+                    ans += right - left
+                    right -= 1
+                else:
+                    left += 1
+        return ans
--- lintcode_57_3sum.py
+++ lintcode_57_3sum.py
@@ -1,33 +1,29 @@
-Python 3.8.3 (default, May 19 2020, 06:50:17) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> class Solution:
+class Solution:
     """
     @param numbers: Give an array numbers of n integer
     @return: Find all unique triplets in the array which gives the sum of zero.
     """
-    def threeSum(self, numbers):
-        # write your code here
-        if len(numbers) < 3:
-            return []
+    def threeSum(self, nums):
+        nums = sorted(nums)
         
-        numbers.sort()
-        res = []
-        
-        for i in range(len(numbers)):
+        results = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            self.find_two_sum(nums, i + 1, len(nums) - 1, -nums[i], results)
             
-            left = i + 1
-            right = len(numbers)-1
-            
-            while left < right:
-                
-                sum_ = numbers[i] + numbers[left] + numbers[right]
-                
-                if sum_ == 0 and [numbers[i], numbers[left], numbers[right]] not in res:
-                    res.append([numbers[i], numbers[left], numbers[right]])
-                    
-                if sum_ >= 0:
-                    right -= 1
-                else:
-                    left += 1
-            
-        return res
+        return results
+
+    def find_two_sum(self, nums, left, right, target, results):
+        last_pair = None
+        while left < right:
+            if nums[left] + nums[right] == target:
+                if (nums[left], nums[right]) != last_pair:
+                    results.append([-target, nums[left], nums[right]])
+                last_pair = (nums[left], nums[right])
+                right -= 1
+                left += 1
+            elif nums[left] + nums[right] > target:
+                right -= 1
+            else:
+                left += 1
