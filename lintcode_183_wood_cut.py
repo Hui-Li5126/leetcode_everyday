e25d318882228c63937a5289bb6774d8ff020653
Hui-Li5126 <lihui5126@gmail.com>
2021-03-21 20:25:25 -0700

    adding new one

---
+++ lintcode_183_wood_cut.py
@@ -0,0 +1,32 @@
+Python 3.8.3 (default, May 19 2020, 06:50:17) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> class Solution:
+    """
+    @param L: Given n pieces of wood with length L[i]
+    @param k: An integer
+    @return: The maximum length of the small pieces
+    """
+    def woodCut(self, L, k):
+        if not L:
+            return 0
+
+        start, end = 1, max(L)
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if self.get_pieces(L, mid) >= k:
+                start = mid
+            else:
+                end = mid
+                
+        if self.get_pieces(L, end) >= k:
+            return end
+        if self.get_pieces(L, start) >= k:
+            return start
+            
+        return 0
+        
+    def get_pieces(self, L, length):
+        pieces = 0
+        for l in L:
+            pieces += l // length
+        return pieces
