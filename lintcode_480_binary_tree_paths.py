a5340161ed2dad2c6797d84a9a2f6efa16c16cb4
Hui-Li5126 <lihui5126@gmail.com>
2021-03-25 21:40:34 -0700

    adding new one

---
+++ lintcode_480_binary_tree_paths.py
@@ -0,0 +1,37 @@
+Python 3.8.3 (default, May 19 2020, 06:50:17) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> """
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: the root of the binary tree
+    @return: all root-to-leaf paths
+    """
+    def binaryTreePaths(self, root):
+        if root is None:
+            return []
+            
+        result = []
+        self.dfs(root, [str(root.val)], result)
+        return result
+
+    def dfs(self, node, path, result):
+        if node.left is None and node.right is None:
+            result.append('->'.join(path))
+            return
+
+        if node.left:
+            path.append(str(node.left.val))
+            self.dfs(node.left, path, result)
+            path.pop()  # 回溯
+        
+        if node.right:
+            path.append(str(node.right.val))
+            self.dfs(node.right, path, result)
+            path.pop()
