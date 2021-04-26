fc23f5187a4e4365504ff0f486db412eb6588cb5
Hui-Li5126 <lihui5126@gmail.com>
2021-04-25 20:14:20 -0700

    adding new one

---
+++ lintcode_69_binary tree level order traversal.py
@@ -0,0 +1,37 @@
+Python 3.8.3 (default, May 19 2020, 06:50:17) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from collections import deque
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: Level order a list of lists of integer
+    """
+    def levelOrder(self, root):
+        if root is None:
+            return []
+            
+        queue = deque([root])
+        result = []
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level)
+        return result
+
+#don't understand the requirement of output
+#don't understand when list denotes a linked list, what is saved in the list, the value or reference of next 
