e9692e1bf153314b45d99a7625df924c0a2a754c
Hui Li <lihui5126@gmail.com>
2023-03-27 19:15:58 -0700

    Add files via upload

--- p70_climbing stairs.py
+++ p70_climbing stairs.py
@@ -1,9 +1,22 @@
-Python 3.8.3 (default, May 19 2020, 06:50:17) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> class Solution:
+class Solution:
     def climbStairs(self, n: int) -> int:
 #climbStairs use recursive will exceed the time limit
         prev, current =0, 1
         for i in range(n):
             prev, current=current, prev+current
         return current
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n < 3:
+            return n
+        
+        dp = [0]*n
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2, n):
+            dp[i] = dp[i - 1] + dp[i -2]
+            
+        return dp[n-1]
+    
